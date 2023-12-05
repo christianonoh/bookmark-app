6dbcaf78ad65070ffad5753ba6695a4aeb7c011c
Christian Onoh <chibyk5000@gmail.com>
2023-12-05 23:42:12 +0100

    Update FAQ section styling

--- components/faq/index.tsx
+++ components/faq/index.tsx
@@ -5,7 +5,7 @@ import Button from "../shared/Button";
 
 const FAQ = () => {
   return (
-    <section className="flex flex-col py-16 px-5 md:px-10  max-w-lg mx-auto items-center">
+    <section className="flex flex-col py-16 px-5 md:px-10  max-w-3xl mx-auto items-center">
       <h2 className="text-3xl capitalized font-bold mb-8 text-center">
         Frequently Asked Questions
       </h2>
@@ -15,17 +15,15 @@ const FAQ = () => {
       </p>
       <div className="w-full">
         <div className="border-b w-full border-gray-light" />
-        {
-          faqs.map((faq, index) => (
-            <Question key={index} question={faq.question} answer={faq.answer} />
-          ))
-        }
+        {faqs.map((faq, index) => (
+          <Question key={index} question={faq.question} answer={faq.answer} />
+        ))}
       </div>
       <Button
-            title="More Info"
-            link="/"
-            className="font-bold shadow-md hover:text-accent hover:border-accent hover:bg-transparent flex-1 mt-8"
-          />
+        title="More Info"
+        link="/"
+        className="font-bold shadow-md hover:text-accent hover:border-accent hover:bg-transparent flex-1 mt-8"
+      />
     </section>
   );
 };
