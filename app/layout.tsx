25897e0d520194a14dc7855711d816781398257e
Christian Onoh <chibyk5000@gmail.com>
2023-12-05 10:04:08 +0100

    Setup project

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Bookmark app',
+  description: 'A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+      <link
+        rel="icon"
+        type="image/png"
+        href="/images/favicon-32x32.png"
+      />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
