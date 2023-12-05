9ea1ce2a8c7033738a650f0d2b3a9ca3829912d8
Christian Onoh <chibyk5000@gmail.com>
2023-12-05 16:14:17 +0100

    Update Button component styles

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -13,14 +13,14 @@ const Button = ({ title, link, className, external }: ButtonProps) => {
   if (external) {
     return (
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <button className={cx('px-6 py-3 rounded-md bg-accent text-white font-medium text-xs border border-transparent transition-all duration-200 ease-in-out',className)}>{title}</button>
+        <button className={cx('px-4 md:px-6 py-3 rounded-md whitespace-nowrap bg-accent text-white font-bold text-sm border-2 border-transparent transition-all duration-200 ease-in-out',className)}>{title}</button>
       </a>
     );
   }
 
   return (
     <Link href={link}>
-        <button className={cx('px-6 py-3 rounded-md bg-accent text-white font-semibold text-xs border border-transparent transition-all duration-200 ease-in-out',className)}>{title}</button>
+        <button className={cx('px-4 md:px-6 py-3 rounded-md whitespace-nowrap bg-accent text-white font-bold text-sm border-2 border-transparent transition-all duration-200 ease-in-out',className)}>{title}</button>
     </Link>
   )
 }
