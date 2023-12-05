c0b3a01df5638eab9d26e63b470528d0d6375932
Christian Onoh <chibyk5000@gmail.com>
2023-12-05 19:38:18 +0100

    Update font size and padding in FeaturesSection

--- components/features/index.tsx
+++ components/features/index.tsx
@@ -13,7 +13,7 @@ const FeaturesSection = () => {
   }
   return (
     <section className='flex flex-col py-16 items-center'>
-      <h2 className="text-4xl capitalized font-bold mb-8">Features</h2>
+      <h2 className="text-3xl capitalized font-bold mb-8">Features</h2>
       <p className="text-lg text-gray-medium leading-relaxed mb-16 max-w-lg text-center px-5">
       Our aim is to make it quick and easy for you to access your favourite websites. 
   Your bookmarks sync between your devices so you can access them on the go.
@@ -22,7 +22,7 @@ const FeaturesSection = () => {
       <div className="border-b border-gray-light w-full md:hidden" />
       {
         features.map((feature, index) => (
-          <div key={index} className={cx('flex-1 text-center px-12  py-6 cursor-pointer whitespace-nowrap', selectedFeature === feature.key ? 'border-b-4 border-red text-dark font-bold' : 'text-gray-medium border-b border-gray-light')} onClick={() => handleClick(feature.key)}>
+          <div key={index} className={cx('flex-1 text-center px-12  py-4 md:py-6 cursor-pointer whitespace-nowrap hover:text-red transition-all duration-200 ease-in-out', selectedFeature === feature.key ? 'border-b-4 border-red text-dark font-bold' : 'text-gray-medium border-b border-gray-light')} onClick={() => handleClick(feature.key)}>
             {feature.title}
           </div>
         ))
