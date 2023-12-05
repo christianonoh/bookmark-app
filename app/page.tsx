931fcaded8534af462f96aaad68a81ad6eb08151
Christian Onoh <chibyk5000@gmail.com>
2023-12-05 18:03:27 +0100

    Add FeaturesSection component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturesSection from '@/components/features'
 import HomeCover from '@/components/home-cover'
 
 export default function Home() {
   return (
     <main>
       <HomeCover />
+      <FeaturesSection />
     </main>
   )
 }
