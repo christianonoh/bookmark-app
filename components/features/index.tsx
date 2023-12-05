bab7df8ad9281a5bbb978c904d9b40922c366441
Christian Onoh <chibyk5000@gmail.com>
2023-12-05 23:42:26 +0100

    Refactor features section component

--- components/features/index.tsx
+++ components/features/index.tsx
@@ -1,43 +1,58 @@
-"use client"
+"use client";
 
-import { cx, features } from "@/utils"
-import { useState } from "react"
-import Feature from "./Feature"
+import { cx, features } from "@/utils";
+import { useState } from "react";
+import Feature from "./Feature";
 
 const FeaturesSection = () => {
-  const [selectedFeature, setSelectedFeature] = useState(1)
+  const [selectedFeature, setSelectedFeature] = useState(1);
 
   const handleClick = (feature: number) => {
-    setSelectedFeature(feature)
+    setSelectedFeature(feature);
     console.log(selectedFeature);
-  }
+  };
   return (
-    <section className='flex flex-col py-16 items-center'>
+    <section className="flex flex-col py-16 items-center">
       <h2 className="text-3xl capitalized font-bold mb-8">Features</h2>
       <p className="text-lg text-gray-medium leading-relaxed mb-16 max-w-lg text-center px-5">
-      Our aim is to make it quick and easy for you to access your favourite websites. 
-  Your bookmarks sync between your devices so you can access them on the go.
-    </p>
-    <div className="flex flex-col md:flex-row px-5 mb-16 w-full">
-      <div className="border-b border-gray-light w-full md:hidden" />
-      {
-        features.map((feature, index) => (
-          <div key={index} className={cx('flex-1 text-center px-12  py-4 md:py-6 cursor-pointer whitespace-nowrap hover:text-red transition-all duration-200 ease-in-out', selectedFeature === feature.key ? 'border-b-4 border-red text-dark font-bold' : 'text-gray-medium border-b border-gray-light')} onClick={() => handleClick(feature.key)}>
+        Our aim is to make it quick and easy for you to access your favourite
+        websites. Your bookmarks sync between your devices so you can access
+        them on the go.
+      </p>
+      <div className="flex flex-col md:flex-row px-5 mb-16 w-full max-w-3xl mx-auto">
+        <div className="border-b border-gray-light w-full md:hidden" />
+        {features.map((feature, index) => (
+          <div
+            key={index}
+            className={cx(
+              "flex-1 text-center px-12  py-4 md:py-6 cursor-pointer whitespace-nowrap hover:text-red transition-all duration-200 ease-in-out",
+              selectedFeature === feature.key
+                ? "border-b-4 border-red text-dark font-medium"
+                : "text-gray-medium border-b border-gray-light"
+            )}
+            onClick={() => handleClick(feature.key)}
+          >
             {feature.title}
           </div>
-        ))
-      }
-    </div>
-    {
-        features.map((feature, index) => (
-          <div key={index} className={cx('w-full', selectedFeature === feature.key ? 'block' : 'hidden')}>
-            <Feature img={feature.img} title={feature.title} content={feature.content} />
-          </div>
-        ))
-      }
-
+        ))}
+      </div>
+      {features.map((feature, index) => (
+        <div
+          key={index}
+          className={cx(
+            "w-full",
+            selectedFeature === feature.key ? "block" : "hidden"
+          )}
+        >
+          <Feature
+            img={feature.img}
+            title={feature.title}
+            content={feature.content}
+          />
+        </div>
+      ))}
     </section>
-  )
-}
+  );
+};
 
-export default FeaturesSection
+export default FeaturesSection;
