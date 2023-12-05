00d6e1d13ce2cdf4bdaf9c7e3153f0bf319ebfb3
Christian Onoh <chibyk5000@gmail.com>
2023-12-05 16:13:57 +0100

    Refactor home page to use HomeCover component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import { ArrowDownIcon, CloseIcon, ErrorIcon, FacebookIcon } from '@/components/icons'
-import Image from 'next/image'
+import HomeCover from '@/components/home-cover'
 
 export default function Home() {
   return (
     <main>
-      <ErrorIcon className='h-10 w-10'/>
-      <CloseIcon className='h-10 w-10' fill='#000'/>
-      <FacebookIcon className='h-10 w-10' fill='000'/>
-      <ArrowDownIcon className='h-10 w-10'/>
+      <HomeCover />
     </main>
   )
 }
